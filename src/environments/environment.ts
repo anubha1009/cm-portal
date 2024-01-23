b5faaf5b87134d146018cfc8d7e286c8b205d1b6
“anubha1009” <axv64880@ucmo.edu>
2024-01-23 01:58:52 -0600

    Changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
       // baseUrl:"http://localhost:3000"
-      baseUrl:"https://pmhr2vri7bs622yqnomo7vqxzm0cerug.lambda-url.us-east-2.on.aws"
+      baseUrl:"https://weoith5593.execute-api.us-east-2.amazonaws.com/Dev"
 };
