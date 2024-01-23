4a4af199637520976726af5fe71402a00d32915e
“anubha1009” <axv64880@ucmo.edu>
2024-01-23 01:32:56 -0600

    Changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-      baseUrl:"http://localhost:3000"
+      // baseUrl:"http://localhost:3000"
+      baseUrl:"https://pmhr2vri7bs622yqnomo7vqxzm0cerug.lambda-url.us-east-2.on.aws"
 };
